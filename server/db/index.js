47795921faf7e2963cd6887cafbb1116162b688d
Mary Gordanier <mcgordanier@gmail.com>
2020-06-05 17:30:45 -0400

    add routes to access dog by id and assign owner

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,12 @@
 //import database
 const db = require('./database')
 //import all models
-const Owner = require('./models/owner')
 const Dog = require('./models/dog')
+const Owner = require('./models/owner')
 
 //make associations between models
 Owner.hasMany(Dog)
 Dog.belongsTo(Owner)
-console.log(Object.keys(Dog.prototype))
 
 //export db and models for use in other files
-module.exports = { db }
+module.exports = { db, Dog, Owner }
